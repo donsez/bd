56cf414d3dbd14ea27e0219618c78e59d2b9a4d4
Didier Donsez <didier.donsez@gmail.com>
2024-09-27 18:41:21 +0200

    improve example

--- postgres/work/bank/bank.sql
+++ postgres/work/bank/bank.sql
@@ -24,19 +24,45 @@ CREATE TABLE transfer (
 
 -- Insert rows
 
+\! echo "Create 2 new accounts"
+
+BEGIN;
 INSERT INTO account (name) VALUES ('alice');
 INSERT INTO account (name) VALUES ('bob');
-
-
-UPDATE account SET balance = balance+1000 WHERE name = 'alice';
-INSERT INTO transfer (account_to, amount, reason) VALUES ('alice', 1000, 'initial deposit');
-
-UPDATE account SET balance = balance+500 WHERE name = 'bob';
-INSERT INTO transfer (account_to, amount, reason) VALUES ('bob', 500, 'initial deposit');
-
+COMMIT;
 
 SELECT * FROM account;
 SELECT * FROM transfer;
+
+\! echo "Initial deposit by Alice"
+
+BEGIN;
+UPDATE account SET balance = balance+1000 WHERE name = 'alice';
+INSERT INTO transfer (account_to, amount, reason) VALUES ('alice', 1000, 'initial deposit');
+COMMIT;
+
+SELECT * FROM account;
+SELECT * FROM transfer;
+
+SELECT
+  pg_sleep(5),
+  clock_timestamp();
+
+\! echo "Initial deposit by Bob"
+
+BEGIN;
+UPDATE account SET balance = balance+500 WHERE name = 'bob';
+INSERT INTO transfer (account_to, amount, reason) VALUES ('bob', 500, 'initial deposit');
+COMMIT;
+
+SELECT * FROM account;
+SELECT * FROM transfer;
+
+SELECT
+  pg_sleep_for('5 seconds'),
+  clock_timestamp();
+
+\! echo "Transfer 100 from Alice to Bob"
 
 BEGIN;
 UPDATE account SET balance = balance-100 WHERE name = 'alice';
@@ -47,11 +73,38 @@ COMMIT;
 SELECT * FROM account;
 SELECT * FROM transfer;
 
+SELECT
+  pg_sleep_for('5 seconds'),
+  clock_timestamp();
+
+\! echo "Transfer 200 from Bob to Alice"
 
 BEGIN;
-UPDATE account SET balance = balance+200 WHERE name = 'bob';
 UPDATE account SET balance = balance-200 WHERE name = 'alice';
-INSERT INTO transfer (account_from, account_to, amount) VALUES ('bob', 'alice', 200, 'xmas present');
+UPDATE account SET balance = balance+200 WHERE name = 'bob';
+INSERT INTO transfer (account_from, account_to, amount) VALUES ('alice', 'bob', 200, 'new year day refund');
+ROLLBACK;
+
+SELECT * FROM account;
+SELECT * FROM transfer;
+
+SELECT
+  pg_sleep_for('5 seconds'),
+  clock_timestamp();
+
+\! echo "Transfer 100 from Alice to Bob"
+
+BEGIN;
+UPDATE account SET balance = balance+100 WHERE name = 'bob';
+SELECT * FROM account;
+SAVEPOINT my_savepoint;
+UPDATE account SET balance = balance+200 WHERE name = 'bob';
+SELECT * FROM account;
+ROLLBACK TO SAVEPOINT my_savepoint;
+SELECT * FROM account;
+UPDATE account SET balance = balance-100 WHERE name = 'alice';
+SELECT * FROM account;
+INSERT INTO transfer (account_from, account_to, amount) VALUES ('alice', 'bob', 100, 'easter (chocolate) eggs');
 COMMIT;
 
 SELECT * FROM account;
