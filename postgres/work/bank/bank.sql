47a5f2bcaf95ea7a8fff7f9b3579856e4c7d9ad3
Didier DONSEZ <didier.donsez@gmail.com>
2024-09-27 23:16:06 +0200

    fix prompt

--- postgres/work/bank/bank.sql
+++ postgres/work/bank/bank.sql
@@ -19,7 +19,7 @@ CREATE TABLE transfer (
     account_to TEXT NOT NULL,
     amount NUMERIC(10,2),
     reason TEXT,
-    date DATE DEFAULT CURRENT_DATE
+    date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 -- Insert rows
@@ -77,12 +77,18 @@ SELECT
   pg_sleep_for('5 seconds'),
   clock_timestamp();
 
-\! echo "Transfer 200 from Bob to Alice"
+\! echo "Transfer 200 from Bob to Alice (canceled))"
 
 BEGIN;
 UPDATE account SET balance = balance-200 WHERE name = 'alice';
+SELECT * FROM account;
+SELECT * FROM transfer;
 UPDATE account SET balance = balance+200 WHERE name = 'bob';
-INSERT INTO transfer (account_from, account_to, amount) VALUES ('alice', 'bob', 200, 'new year day refund');
+SELECT * FROM account;
+SELECT * FROM transfer;
+INSERT INTO transfer (account_from, account_to, amount, reason) VALUES ('alice', 'bob', 200, 'new year day refund');
+SELECT * FROM account;
+SELECT * FROM transfer;
 ROLLBACK;
 
 SELECT * FROM account;
@@ -104,9 +110,44 @@ ROLLBACK TO SAVEPOINT my_savepoint;
 SELECT * FROM account;
 UPDATE account SET balance = balance-100 WHERE name = 'alice';
 SELECT * FROM account;
-INSERT INTO transfer (account_from, account_to, amount) VALUES ('alice', 'bob', 100, 'easter (chocolate) eggs');
+INSERT INTO transfer (account_from, account_to, amount, reason) VALUES ('alice', 'bob', 100, 'easter (chocolate) eggs');
 COMMIT;
 
 SELECT * FROM account;
 SELECT * FROM transfer;
 
+BEGIN;
+UPDATE account SET balance = balance+50 WHERE name = 'bob';
+UPDATE account SET balance = balance-50 WHERE name = 'alice';
+INSERT INTO transfer (account_from, account_to, amount, reason) VALUES ('alice', 'bob', 50, 'valetine day present');
+COMMIT;
+
+
+SELECT
+  pg_sleep_for('5 seconds'),
+  clock_timestamp();
+
+-- \prompt 'Please, enter the amount to transfer: ' amount
+\prompt 'Confirm the transfer (Y/N): ' confirm
+
+
+SELECT set_config('psql.confirm', 'confirm', false);
+
+DO $$
+DECLARE
+    c CHAR(1)= current_setting('psql.confirm');
+BEGIN
+    IF c <> 'Y' THEN
+        RAISE NOTICE 'Aborting transfer (%)', c;
+        ROLLBACK;
+    ELSE
+        RAISE NOTICE 'Committing transfer (%)', c;
+        UPDATE account SET balance = balance+50 WHERE name = 'bob';
+        UPDATE account SET balance = balance-50 WHERE name = 'alice';
+        INSERT INTO transfer (account_from, account_to, amount, reason) VALUES ('alice', 'bob', 50, 'father day');
+        COMMIT;
+    END IF;
+END $$;
+
+SELECT * FROM account;
+SELECT * FROM transfer;
